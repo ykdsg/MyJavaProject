032b276de392d943ef7c63e8126ec2f4a4392167
ykdsg <17173as@163.com>
2015-10-27 20:07:37 +0800

    init

--- base/src/main/java/com/hz/yk/yk/io/Main.java
+++
@@ -1,7 +0,0 @@
-package com.hz.yk.yk.io;
-
-/**
- * Created by ykdsg on 15/5/30.
- */
-public class Main {
-}
--- base/src/main/java/com/hz/yk/yk/jmx/Main.java
+++
@@ -1,7 +0,0 @@
-package com.hz.yk.yk.jmx;
-
-/**
- * Created by ykdsg on 15/6/3.
- */
-public class Main {
-}
