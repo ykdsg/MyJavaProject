63de44a45a757d96a80e9708cde619ec8c9cc5df
ykdsg <17173as@163.com>
2015-12-25 17:22:39 +0800

    quartz 简单示例

---
+++ base/src/main/java/com/hz/yk/yk/stack/T2.java
@@ -0,0 +1,18 @@
+package com.hz.yk.yk.stack;
+
+/**
+ * Created by wuzheng.yk on 15/12/23.
+ */
+public class T2 {
+    public void t2Method(){
+        getCaller();
+    }
+    public  void  getCaller(){
+        StackTraceElement stack[] = Thread.currentThread().getStackTrace();
+        for (StackTraceElement ste:stack){
+            if((ste.getClassName().indexOf("T1"))!=-1){
+                System.out.println("called by "+ste.getClassName()+"."+ste.getMethodName()+"/"+ste.getFileName());
+            }
+        }
+    }
+}
