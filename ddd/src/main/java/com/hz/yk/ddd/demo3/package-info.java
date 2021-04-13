5e2914805e664a3b4ef7b75482d87590f6023af4
wuzheng <17173as@163.com>
2021-04-13 15:00:01 +0800

    增加DDD相关代码说明

--- ddd/src/main/java/com/hz/yk/ddd/demo3/package-info.java
+++ ddd/src/main/java/com/hz/yk/ddd/demo3/package-info.java
@@ -1,5 +1,6 @@
 /**
  * https://juejin.cn/post/6845166890554228744
+ * 阿里技术专家详解DDD系列 第三讲 - Repository模式
  *
  * @author wuzheng.yk
  * @date 2021/1/21
--- ddd/src/main/java/com/hz/yk/ddd/demo4/package-info.java
+++ ddd/src/main/java/com/hz/yk/ddd/demo4/package-info.java
@@ -1,5 +1,6 @@
 /**
- * https://juejin.cn/post/6912228908075057166#heading-1
+ * https://juejin.cn/post/6912228908075057166
+ * DDD系列第四讲：领域层设计规范
  *
  * @author wuzheng.yk
  * @date 2021/1/26
