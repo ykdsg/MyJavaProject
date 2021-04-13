11cfe976c994f4a5c1edaf6f02b696d50a06755a
wuzheng <17173as@163.com>
2021-04-13 14:54:08 +0800

    webmagic相关demo

---
+++ webmagic/src/main/java/com/hz/yk/webmagic/WebMagicDemo.java
@@ -0,0 +1,38 @@
+package com.hz.yk.webmagic;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.ConsolePipeline;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+import java.util.List;
+
+/**
+ * @author wuzheng.yk
+ * @date 2021/4/8
+ */
+public class WebMagicDemo implements PageProcessor {
+
+    private Site site = Site.me().setDomain("my.oschina.net");
+
+    @Override
+    public void process(Page page) {
+        List<String> links = page.getHtml().links().regex("http://my\\.oschina\\.net/flashsword/blog/\\d+").all();
+        page.addTargetRequests(links);
+        page.putField("title",
+                      page.getHtml().xpath("//div[@class='BlogEntity']/div[@class='BlogTitle']/h1").toString());
+        page.putField("content", page.getHtml().$("div.content").toString());
+        page.putField("tags", page.getHtml().xpath("//div[@class='BlogTags']/a/text()").all());
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+
+    public static void main(String[] args) {
+        Spider.create(new WebMagicDemo()).addUrl("http://my.oschina.net/flashsword/blog")
+                .addPipeline(new ConsolePipeline()).run();
+    }
+}
