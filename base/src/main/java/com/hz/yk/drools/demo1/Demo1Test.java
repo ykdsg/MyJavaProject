16358a617c0a0910d8acc5d8f16f98730eaad540
wuzheng <17173as@163.com>
2021-06-08 16:50:32 +0800

    drools基本用法

---
+++ base/src/main/java/com/hz/yk/drools/demo1/Demo1Test.java
@@ -0,0 +1,58 @@
+package com.hz.yk.drools.demo1;
+
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.io.ResourceType;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.utils.KieHelper;
+
+/**
+ * @author wuzheng.yk
+ * @date 2021/6/8
+ */
+public class Demo1Test {
+
+    private static KieContainer container = null;
+    private KieSession statefulKieSession = null;
+
+    @Test
+    public void test() {
+        KieServices kieServices = KieServices.Factory.get();
+        container = kieServices.getKieClasspathContainer();
+        statefulKieSession = container.newKieSession("demo1-rules");
+        Person person = new Person();
+
+        person.setAge(12);
+        person.setName("Test");
+
+        statefulKieSession.insert(person);
+        statefulKieSession.fireAllRules();
+        statefulKieSession.dispose();
+    }
+
+    @Test
+    public void testRuleString() {
+        String myRule =
+                "import com.hz.yk.drools.demo1.Person\n" + "\n" + "dialect  \"mvel\"\n" + "\n" + "rule \"age\"\n"
+                + "    when\n" + "        $person : Person(age<16 || age>50)\n" + "    then\n"
+                + "        System.out.println(\"这个人的年龄不符合要求！（基于动态加载）\");\n" + "end\n";
+
+        KieHelper helper = new KieHelper();
+
+        helper.addContent(myRule, ResourceType.DRL);
+
+        KieSession ksession = helper.build().newKieSession();
+
+        Person person = new Person();
+
+        person.setAge(12);
+        person.setName("Test");
+
+        ksession.insert(person);
+
+        ksession.fireAllRules();
+
+        ksession.dispose();
+    }
+}
---
+++ base/src/main/java/com/hz/yk/drools/demo1/Person.java
@@ -0,0 +1,28 @@
+package com.hz.yk.drools.demo1;
+
+/**
+ * @author wuzheng.yk
+ * @date 2021/6/8
+ */
+public class Person {
+
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
